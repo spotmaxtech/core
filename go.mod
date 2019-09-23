5e289455e86e691409e28a370707575972568630
Liuzoxan <liuzoxan@gmail.com>
2019-09-23 15:07:09 +0800

    add tags

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/spotmaxtech/core
+
+go 1.12
---
+++ tags.go
@@ -0,0 +1,6 @@
+package core
+
+const (
+	MAXGROUP_SpotMax       = "spotmax:group:maxgroup"
+	MAXGROUP_AutoRecommend = "spotmax:group:auto-recommend"
+)
